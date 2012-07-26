68311be84fa38ddafc764ee315ae39681dcff587
S.V. Paulauskas <stanpaulauskas@gmail.com>
2012-07-26 19:04:08 -0400

    Creates output to copy directly to cal.txt Made things self-contained. Fixed error with label printing. Moved readme to plain text.
    README.md -> README
    Updated the readme to have the latest ifnormation on how to run things.
    
    fitAll.bash -> energyCal.bash
    Updated so that it no longer requires the external function call to the
    makePdf script.  It will also set the position of the fitted equation to
    the maximum gamma energy in the source file. Finally, it will also now
    create a series of files formatted to be copied directly into a cal.txt.
    
    plot.gp
    Changes reflect those mentioned before for the label printing. Also, fixed
    some margins so that things do not have so much negative space around the
    plots.
    
    Signed-off-by: S.V. Paulauskas <stanpaulauskas@gmail.com>

--- energyCal.bash
+++ energyCal.bash
@@ -4,15 +4,29 @@
 # A script to do energy calibrations and gain matching
 #  for clover data taken at LeRIBSS
 # Associated File: plot.gp
+
+#Define some of the directories
 source=$1
 folderName=$2
 dataDir="$2/data"
 resDir="$2/results"
+calDir="$2/cals"
 
+function makePdf {
+    gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=${psFiles[0]} -dBATCH \
+	-dPDFFitPage $(
+	for i in $(seq $(expr ${#psFiles[@]} - 1) -1 1) 
+	do  
+	    echo -n "${psFiles[$i]} " 
+	    done)
+}
+
+#The loop for the first run for a calibration
 if [ ! -d $folderName ]
 then
     mkdir -p $dataDir
     mkdir -p $resDir
+    mkdir -p $calDir
     for i in `seq 1 $1`
     do
 	for j in 0 1 2 3
@@ -24,14 +38,21 @@ then
     exit
 fi
 
+#Do some preliminary cleanup and run the main body.
 rm -f temp.log folderName/fitResults.* fit.log 
 
+#find the maximum source energy
 currentSource=$(echo "source='$source'")
 sed '9c\'$currentSource plot.gp > plot.temp
 mv plot.temp plot.gp
 
 currentFolder=$(echo "folderName='$folderName'")
 sed '10c\'$currentFolder plot.gp > plot.temp
+mv plot.temp plot.gp
+
+maxEnergy=`sort -k2 -n $source | tail -1 | awk '{print $2}'`
+labelYPos=$(echo "labelYPos='$maxEnergy'")
+sed '11c\'$labelYPos plot.gp > plot.temp
 mv plot.temp plot.gp
 
 for file in $folderName/data/*.dat
@@ -43,9 +64,9 @@ do
     currentClover=$(echo "clover='$clover'")
     currentLeaf=$(echo "leaf='$leaf'")
     
-    sed '11c\'$currentClover plot.gp > plot.temp
+    sed '12c\'$currentClover plot.gp > plot.temp
     mv plot.temp plot.gp
-    sed '12c\'$currentLeaf plot.gp > plot.temp
+    sed '13c\'$currentLeaf plot.gp > plot.temp
     mv plot.temp plot.gp
 
     for fit in cube lin quad
@@ -56,28 +77,28 @@ do
 	then
 	    fitFunc='f(x)=a*x**3+b*x**2+c*x+d'
 	    fitCommand="fit f(x) fileCommand(source,folderName, clover,leaf) u 3:2 via a,b,c,d"
-	    label='set label 1 sprintf("f(x) = %g (1/keV^2) x^3 + %g (1/keV) x^2 + %g keV x + %g keV",a,b,c,d) at 500, 4000'
+	    label='set label 1 sprintf("f(x) = %g (1/keV^2) x^3 + %g (1/keV) x^2 + %g keV x + %g keV",a,b,c,d) at 250, labelYPos'
 	elif [[ $fit == lin ]]
 	then
 	    fitFunc='f(x)=a*x+b'
 	    fitCommand="fit f(x) fileCommand(source,folderName, clover,leaf) u 3:2 via a,b"
-	    label='set label 1 sprintf("f(x) = %g keV x + %g keV",a,b) at 500, 4000'
+	    label='set label 1 sprintf("f(x) = %g keV x + %g keV",a,b) at 250, labelYPos'
 	else
 	    fitFunc='f(x)=a*x**2+b*x+c'
 	    fitCommand="fit f(x) fileCommand(source,folderName, clover,leaf) u 3:2 via a,b,c"
-	    label='set label 1 sprintf("f(x) = %g (1/keV) x^2 + %g keV x + %g keV",a,b,c) at 500, 4000'
+	    label='set label 1 sprintf("f(x) = %g (1/keV) x^2 + %g keV x + %g keV",a,b,c) at 250, labelYPos'
 	fi
 
-	sed '13c\'$fitType plot.gp > plot.temp
+	sed '14c\'$fitType plot.gp > plot.temp
 	mv plot.temp plot.gp
 
-	sed '24c\'$fitFunc plot.gp > plot.temp
+	sed '25c\'$fitFunc plot.gp > plot.temp
 	mv plot.temp plot.gp
 
-	sed '26c\'"$fitCommand" plot.gp > plot.temp
+	sed '27c\'"$fitCommand" plot.gp > plot.temp
 	mv plot.temp plot.gp
 	
-	sed '28c\'"$label" plot.gp > plot.temp
+	sed '29c\'"$label" plot.gp > plot.temp
 	mv plot.temp plot.gp
 	
 	gnuplot plot.gp
@@ -88,14 +109,37 @@ do
 	    >> fitResults-"$fit".dat
     
 	cat temp.log >> fitResults-"$fit".dat
-    #awk '{if($2=="=") print $0}' temp.log >> fitResults.dat
 	echo -e "\n" >> fitResults-"$fit".dat
     done
 done
-rm temp.log
+
 for fit in quad lin cube
 do
-    ,makePdf *-"$fit".ps fitResults-"$fit".pdf
+    declare -a psFiles=(fitResults-"$fit".pdf *-"$fit".ps)
+    makePdf psFiles
+        
+    file=fitResults-$fit.dat
+    temp=`awk '{if($2=="=") print $1,$3}' $file`
+
+    echo $temp | tr 'a' '\n'> temp.out
+    while read line 
+    do 
+	set -- $line
+	
+	if [[ $fit == "lin" ]]
+	then
+	    echo $3 $1 >> cals-lin.dat
+	elif [[ $fit == "quad" ]]
+	then
+	    echo $5 $3 $1 >> cals-quad.dat
+	elif [[ $fit == "cube" ]]
+	then
+	    echo $7 $5 $3 $1 >> cals-cube.dat
+	fi
+    done < temp.out
 done
 mv fitResults-*.* $resDir
-rm *.ps
+mv cals-*.dat $calDir
+rm *.ps temp.out temp.log
+
+
--- plot.gp
+++ plot.gp
@@ -4,10 +4,11 @@
 #  perform fits on data taken from LeRIBSS
 #Associated file: fitAll.bash
 reset
-set terminal wxt enhanced
+#set terminal wxt enhanced
 
-source='sourceFiles/73cu.dat'
-folderName='73cu'
+source='3'
+folderName='test00'
+labelYPos=''
 clover='clover3'
 leaf='leaf3'
 fitType='quad'
@@ -25,7 +26,7 @@ f(x)=a*x**2+b*x+c
 
 fit f(x) fileCommand(source,folderName, clover,leaf) u 3:2 via a,b,c
 
-set label 1 sprintf("f(x) = %g (1/keV) x^2 + %g keV x + %g keV",a,b,c) at 500, 4000
+set label 1 sprintf("f(x) = %g (1/keV) x^2 + %g keV x + %g keV",a,b,c) at 250, labelYPos
 set title graphTitle(clover,leaf,source)
 set ylabel offset 2,0 "Energy (keV)"
 unset key
@@ -33,14 +34,20 @@ unset key
 set terminal postscript enhanced color solid "Helvetica" 12
 set output psName(clover,leaf,fitType)
 
+set lmargin 1
+
 set multiplot
+set tmargin 0
 set size 1,0.6
 set origin 0,0.4
 plot fileCommand(source,folderName,clover,leaf) u 3:2, f(x)
 
 set xlabel "Centroid (arb)"
 unset title
+unset label 1
 set yrange [-1.5:1.5]
+set tmargin 0
+set bmargin 0
 set size 1,0.4
 set origin 0,0.0
 plot fileCommand(source,folderName,clover,leaf) u 3:($2-f($3)), 0 lc -1
